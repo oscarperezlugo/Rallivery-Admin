e071932af6e8de54d09051700be2426888ed19fa
oscarperezlugo <56516855+oscarperezlugo@users.noreply.github.com>
2021-05-28 17:23:39 -0400

    Initial commit

---
+++ Site.Master.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PanelAdmin
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["nombreC"] != null)
+            {
+                Label1.Text = "" + Request.Cookies["nombreC"].Value + " " + Request.Cookies["appellC"].Value + "";
+                if(Request.Cookies["perfilC"] != null)
+                {
+                    if (Request.Cookies["perfilC"].Value == "ADMIN")
+                    {
+
+                    }
+                    else if (Request.Cookies["perfilC"].Value == "VENDEDOR")
+                    {
+                        bloque2.Attributes.Add("style", "display:none");
+                        //bloque3.Attributes.Add("style", "display:none");
+                        bloque4.Attributes.Add("style", "display:none");
+                    }
+                    else if (Request.Cookies["perfilC"].Value == "CONTADOR")
+                    {
+                        bloque2.Attributes.Add("style", "display:none");
+                        //bloque3.Attributes.Add("style", "display:none");
+                        bloque1.Attributes.Add("style", "display:none");
+                        bloque5.Attributes.Add("style", "display:none");
+                    }
+                    else if (Request.Cookies["perfilC"].Value == "SERVICIO")
+                    {
+                        bloque4.Attributes.Add("style", "display:none");
+                        //bloque3.Attributes.Add("style", "display:none");
+                        bloque1.Attributes.Add("style", "display:none");
+                        bloque5.Attributes.Add("style", "display:none");
+                    }
+                    else
+                    {
+
+                        Page.RegisterStartupScript("LoadFunction", "<script type='text/javascript'>alert('NO AUTORIZADO');window.location='Login.aspx'</script>");
+                    }
+                }
+                else
+                {
+                    Page.RegisterStartupScript("LoadFunction", "<script type='text/javascript'>alert('NO AUTORIZADO');window.location='Login.aspx'</script>");
+                }
+
+            }
+            else
+            {
+                Page.RegisterStartupScript("LoadFunction", "<script type='text/javascript'>alert('NO AUTORIZADO');window.location='Login.aspx'</script>");
+            }
+            
+        }
+
+        protected void CierreClick(object sender, EventArgs e)
+        {
+            if (HttpContext.Current != null)
+            {
+                int cookieCount = HttpContext.Current.Request.Cookies.Count;
+                for (var i = 0; i < cookieCount; i++)
+                {
+                    var cookie = HttpContext.Current.Request.Cookies[i];
+                    if (cookie != null)
+                    {
+                        var expiredCookie = new HttpCookie(cookie.Name)
+                        {
+                            Expires = DateTime.Now.AddDays(-1),
+                            Domain = cookie.Domain
+                        };
+                        HttpContext.Current.Response.Cookies.Add(expiredCookie); // overwrite it
+                    }
+                }
+
+                // clear cookies server side
+                HttpContext.Current.Request.Cookies.Clear();
+            }
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
---
+++ Site.Master.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PanelAdmin
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control MainContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+
+        /// <summary>
+        /// Control bloque5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bloque5;
+
+        /// <summary>
+        /// Control bloque1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bloque1;
+
+        /// <summary>
+        /// Control bloque2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bloque2;
+
+        /// <summary>
+        /// Control bloque4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bloque4;
+    }
+}
